584c9012fe8b737f8c145c332e795303735290f2
Halil Özcan <halil_ozcann@outlook.com>
2024-12-23 21:28:46 +0300

    C# OOP Sinif Proje Yapıldı

---
+++ OOPSınıfProje/Arac.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSınıfProje
+{
+    public class Arac
+    {
+        public string Marka;
+        public string Model;
+        public int ModelYil;
+        public decimal Km;
+        public string YakitTipi;
+        public int VitesTipi;
+        public decimal AlisFiyati;
+        public decimal SatisFiyati;
+        public decimal MaxİndirimTutar;
+        public decimal Fiyat;
+        
+
+        public Arac() { }
+
+        public Arac(string _marka, string _model)
+        {
+            Marka = _marka;
+            Model = _model;
+           
+           
+        }
+
+        public Arac(string _marka, string _model, int _modelYil)
+        {
+            Marka = _marka;
+            Model = _model;
+            ModelYil = _modelYil;
+        }
+
+        public Arac(string _marka, string _model, int _modelYil, decimal _km)
+        {
+            Marka = _marka;
+            Model = _model;
+            ModelYil = _modelYil;
+            Km = _km;
+        }
+
+
+        public void BilgiGoster()
+        {
+            Console.WriteLine("Marka {0} - Model {1} - ModelYil{2}", Marka, Model, ModelYil); 
+        }
+
+        public void fiyatAta(decimal _fiyat)
+        {
+            decimal fiyatHesap = SatisFiyati - MaxİndirimTutar;
+            if(_fiyat<fiyatHesap)
+            {
+                Console.WriteLine("Geçersiz Fiyat Girişi");
+            }
+            else
+            {
+                this.Fiyat = _fiyat;
+                Console.WriteLine("Fiyat Bilgisi Güncellendi");
+            }
+        }
+    }
+}
--- OOPSınıfProje/Program.cs
+++ OOPSınıfProje/Program.cs
@@ -11,6 +11,16 @@ namespace OOPSınıfProje
         static void Main(string[] args)
         {
 
+            Arac arac = new Arac("Opel", "Corsa",2010, 56000);
+
+            arac.AlisFiyati = 45000;
+            arac.AlisFiyati = 60000;
+            arac.MaxİndirimTutar = 10000;
+
+            arac.fiyatAta(50000);
+            arac.BilgiGoster();
+
+           
 
         }
     }
