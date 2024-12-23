8bcb357b5fc5f505a6e1cda5c25f2cc5d3259bed
Halil Özcan <halil_ozcann@outlook.com>
2024-12-23 21:38:01 +0300

    C# OOP Sinif Proje 2 Yapıldı

---
+++ OOPSinifProje2/Kitap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSinifProje2
+{
+    public class Kitap
+    {
+        string KitapAdi;
+        string Yazar;
+        string sayfaSayisi;
+
+
+        public Kitap(string kitapAdi, string yazar, string sayfaSayisi)
+        {
+            KitapAdi = kitapAdi;
+            Yazar = yazar;
+            this.sayfaSayisi = sayfaSayisi;
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Kitabin Adi {0} - Yazar {1} - SayfaSayisi{2}", KitapAdi, Yazar, sayfaSayisi);
+        }
+    }
+}
---
+++ OOPSinifProje2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSinifProje2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap kitap = new Kitap("Martin Eden", "Jack London", "250");
+            Kitap kitap1 = new Kitap("1984", "George Orwell", "400");
+            Kitap kitap2 = new Kitap("Uğultulu Tepeler", "Emily Bronte", "320");
+            
+            kitap.BilgileriGoster();
+            kitap1.BilgileriGoster();
+            kitap2.BilgileriGoster();
+        }
+    }
+}
